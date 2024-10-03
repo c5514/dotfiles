05d4771343cf396c9961661db8e0303ca1f2ad49
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-10-03 17:51:13 -0500

    Add files via upload

--- .dotfiles/home/nixvim/plugins/luasnip.nix
+++ .dotfiles/home/nixvim/plugins/luasnip.nix
@@ -1,21 +1,49 @@
-{pkgs, ...}:
+{pkgs,lib,config, ...}:
 {
-	programs.nixvim.plugins.luasnip = {
-		enable = true;
-		settings = {
-			enable_autosnippets = true;
-			store_selection_keys = "<Tab>";
-			history = true;
-			updateevents = "TextChanged,TextChangedI";
+	programs.nixvim = {
+		plugins.luasnip = {
+			enable = true;
+			settings = {
+				enable_autosnippets = true;
+				store_selection_keys = "<Tab>";
+				history = true;
+				updateevents = "TextChanged,TextChangedI";
+			};
+			fromVscode = [
+				{
+					lazyLoad = true;
+					paths = "${pkgs.vimPlugins.friendly-snippets}";
+				}
+			];
+			fromLua = [
+				{	paths = "~/.dotfiles/home/nixvim/snippets";	}
+			];
 		};
-		fromVscode = [
+		keymaps = lib.mkIf config.programs.nixvim.plugins.luasnip.enable [
 			{
-				lazyLoad = true;
-				paths = "${pkgs.vimPlugins.friendly-snippets}";
+				mode = ["s" "i"] ;
+				key = "<C-e>";
+				action = "<Plug>luasnip-next-choice";
+				options = {
+					silent = true;
+				};
 			}
-		];
-		fromLua = [
-			{	paths = "~/.dotfiles/home/nvim/snippets";	}
+			{
+				mode = ["s" "i"] ;
+				key = "<C-t>";
+				action = "<cmd>lua _G.dynamic_node_external_update(1)<Cr>";
+				options = {
+					noremap = true;
+				};
+			}
+			{
+				mode = ["s" "i"] ;
+				key = "<C-g>";
+				action = "<cmd>lua _G.dynamic_node_external_update(2)<Cr>";
+				options = {
+					noremap = true;
+				};
+			}
 		];
 	};
 }
