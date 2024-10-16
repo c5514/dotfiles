aafbfbca586a63a7cb49d6159d09d7f67b3488f7
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-10-16 11:06:22 -0500

    Add files via upload

--- .dotfiles/home/nixvim/plugins/illustrate.lua
+++ .dotfiles/home/nixvim/plugins/illustrate.lua
@@ -1,7 +1,22 @@
 local illustrate = require('illustrate')
 local illustrate_finder = require('illustrate.finder')
-vim.keymap.set('n', '<leader>is', function() illustrate.create_and_open_svg() end,
-	{ desc = "Create and open a new SVG file with provided name" })
+
+vim.keymap.set('n', '<leader>is', function()
+	local filename = vim.fn.input("[SVG] Filename: ")
+
+	if filename and filename ~= "" then
+		illustrate.create_and_open_svg(filename)
+
+		vim.defer_fn(function()
+			local pathFile = "figures/".. filename .. ".svg"
+			vim.fn.system("chmod +w " .. pathFile)
+		end, 100 )
+	else
+		print("Filename cannot be empty.")
+	end
+end, { desc = "Create and open a new SVG file with provided name and set write permissions" })
+-- vim.keymap.set('n', '<leader>is', function() illustrate.create_and_open_svg() end,
+-- 	{ desc = "Create and open a new SVG file with provided name" })
 vim.keymap.set('n', '<leader>ia', function() illustrate.create_and_open_ai() end,
 	{ desc = "Create and open a new Adobe Illustrator file with provided name" })
 vim.keymap.set('n', '<leader>io', function() illustrate.open_under_cursor() end,
