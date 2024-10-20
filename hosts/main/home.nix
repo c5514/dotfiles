a327d16e83fcdb2c326dc129a82dba66432227dc
c5514 <c5514@c5514.com>
2024-10-20 07:24:42 -0500

    Adding script to hypridle to avoid suspend while playing media

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -17,6 +17,7 @@
 	home.packages = with pkgs; [
 		ripgrep
 		coreutils
+		brillo
 		clang
 		swww
 		megacmd
--- nixModules/home/HYPR/hypridle.nix
+++ nixModules/home/HYPR/hypridle.nix
@@ -1,37 +1,40 @@
-{pkgs, ...}:
-
+{pkgs, lib,...}:
+let suspendScript = pkgs.writeShellScript "suspend-script" ''
+		${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} Playing -q
+		if [ ¿? == 1 ]; then
+			${pkgs.systemd}/bin/systemctl suspend
+		fi
+	'';
+	brillo = lib.getExe pkgs.brillo;
+	timeout = 300;
+in
 {
 	services.hypridle.enable = true;
 	services.hypridle.package = pkgs.hypridle;
 	services.hypridle.settings = {
 		general = {
     		lock_cmd = "pidof hyprlock || hyprlock";# avoid starting multiple hyprlock instances.
-    		before_sleep_cmd = "loginctl lock-session";# lock before suspend.
-    		after_sleep_cmd = "hyprctl dispatch dpms on";# to avoid having to press a key twice to turn on the display.
+    		before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";# lock before suspend.
 		};
 		# Screenlock
 		listener = [
 			{
-    			# HYPRLOCK TIMEOUT
-    			timeout = 600;
-    			# HYPRLOCK ONTIMEOUT
-    			on-timeout = "loginctl lock-session";
+				timeout = timeout - 10;
+				on-timeout = "${brillo} -O; ${brillo} -u 1000000 -S 10";
+				on-resume = "${brillo} -I -u 500000";
 			}
 			# dpms
 			{
     			# DPMS TIMEOUT
-    			timeout = 660;
-    			# DPMS ONTIMEOUT
+    			inherit timeout;
     			on-timeout = "hyprctl dispatch dpms off";
-    			# DPMS ONRESUME
     			on-resume = "hyprctl dispatch dpms on";
 			}
 			# Suspend
 			{
     			# SUSPEND TIMEOUT
-    			timeout = 1800;
-    			#SUSPEND ONTIMEOUT
-    			on-timeout = "systemctl suspend";
+    			timeout = timeout + 10;
+				on-timeout = suspendScript.outPath;
 			}
 		];
 	};
