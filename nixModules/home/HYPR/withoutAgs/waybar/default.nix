cb71abcca810ce03e4f780574874fd26965a3fa3
c5514 <c5514@c5514.com>
2024-10-20 14:08:11 -0500

    Didnt worked last attempt to fix the bug

--- nixModules/home/HYPR/withoutAgs/waybar/default.nix
+++ nixModules/home/HYPR/withoutAgs/waybar/default.nix
@@ -1,8 +1,8 @@
 {
 	imports = [./style.nix];
 	home.file = {
-		"~/.config/waybar/config".source = ./config;
-		"~/.config/waybar/modules.json".source = ./modules.json;
+		".config/waybar/config".source = ./config;
+		".config/waybar/modules.json".source = ./modules.json;
 	};
 	programs.waybar = {
 		enable = true;
