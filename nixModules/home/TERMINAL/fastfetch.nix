42c633ebdac3a4a12ffc37438b417b2f4ad1c64c
c5514 <c5514@c5514.com>
2024-10-20 07:34:02 -0500

    Fastfetch changes

--- nixModules/home/TERMINAL/fastfetch.nix
+++ nixModules/home/TERMINAL/fastfetch.nix
@@ -4,7 +4,7 @@
 		enable = true;
 		settings = {
     logo= {
-        source = "NixOS";
+        source = "Nixos_small";
         padding.right = 1;
         printRemaining = true;
         position = "left";
@@ -28,11 +28,6 @@
         temp = {
             unit = "C";
             ndigits = 2;
-            color = {
-                "green" = "\u001b[38;2;0;255;0m";
-                "yellow" = "\u001b[1;38;2;255;255;0m";
-                "red" = "\u001b[1;38;2;255;0;0m";
-            };
         };
         bar = {
             charElapsed = "=";
@@ -43,78 +38,58 @@
         percent = {
             type = 3;
             ndigits = 2;
-            color = {
-                "green" = "\\u001b[38;2;0;255;0m";
-                "yellow" = "\\u001b[1;38;2;255;255;0m";
-                "red"= "\\u001b[1;38;2;255;0;0m";
-            };
         };
     };
     modules = [
         {
             type = "title";
             key = "󰶫 Account";
-            keyColor = "\\u001b[38;2;255;128;255m";
             format = "{1}@{2}";
         }
         {
             type = "player";
             key = "  󰥠 Player";
-            keyColor = "\\u001b[38;2;255;128;255m";
         }
         {
             type = "media";
             key = "   Media";
-            keyColor = "\\u001b[38;2;255;128;255m";
             format = "{1} - {3}";
         }
         {
             type = "os";
             key = " os";
-            keyColor = "\\u001b[38;2;0;192;255m";
         }
         {
             type = "kernel";
             key = "   Kernel";
-            keyColor = "\\u001b[38;2;0;192;255m";
-            format = "{2} {4}";
         }
         {
             type = "packages";
             key = "  󰏖 Packages";
-            keyColor = "\\u001b[38;2;0;192;255m";
         }
         {
             type= "uptime";
             key = "   Uptime";
-            keyColor = "\\u001b[38;2;0;192;255m";
         }
         {
             type = "terminal";
             key = "   Terminal";
-            keyColor = "\\u001b[38;2;0;192;255m";
-        	format = "{5} (\\u001b[0;38;2;0;255;0m{6}\\u001b[0m)";
         }
         {
             type = "terminalsize";
             key = "    󰲎 TerminalSize";
-            keyColor = "\\u001b[38;2;0;192;255";
         }
         {
             type = "terminalfont";
             key = "     TerminalFont";
-            keyColor = "\\u001b[38;2;0;192;255";
         }
         {
             type = "shell";
             key = "     Shell";
-            keyColor = "\\u001b[38;2;0;192;255";
-            format = "{6} (\\u001b[0;38;2;0;255;0m{4}\\u001b[0m)";
         }
         {
             type = "localip";
             key = "  󰩟 LocalIP";
-            keyColor = "\\u001b[38;2;0;192;255";
             showipv4 = true;
             showipv6 = true;
             compact = true;
@@ -122,139 +97,111 @@
         {
             type = "publicip";
             key = "  󰩠 PublicIP";
-            keyColor = "\\u001b[38;2;0;192;255";
         }
         {
             type = "locale";
             key = "   Locale";
-            keyColor = "\\u001b[38;2;0;192;255";
         }
         {
             type = "de";
             key = " DE";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "lm";
             key = "  󰧨 LM";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "wm";
             key = "   WM";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "theme";
             key = "  󰉼 Theme";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "icons";
             key = "   Icons";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "font";
             key = "   Font";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "cursor";
             key = "  󰆿 Cursor";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "wallpaper";
             key = "  󰸉 Wallpaper";
-            keyColor = "\\u001b[38;2;255;175;0";
         }
         {
             type = "custom";
             format = "󰾰 Computer";
-            outputColor = "\\u001b[38;2;0;255;175";
         }
         {
             type = "board";
             key = "   Board";
-            keyColor = "\\u001b[38;2;0;255;175";
         }
         {
             type = "bios";
             key = "     BIOS";
-            keyColor = "\\u001b[38;2;0;255;175";
-            format = "{5} - {4} (\\u001b[0;38;2;0;255;0m{2}\\u001b[0m)";
         }
         {
             type = "cpu";
             key = "   CPU";
-            keyColor = "\\u001b[38;2;0;255;175";
             temp = true;
         }
         {
             type = "cpuusage";
             key = "    󰓅 Usage";
-            keyColor = "\\u001b[38;2;0;255;175";
         }
         {
             type = "gpu";
             key = "  󰾲 GPU";
-            keyColor = "\\u001b[38;2;0;255;175";
             format = "{1} {2} - {3} {4}";
             temp = true;
         }
         {
             type = "memory";
             key = "   Memory";
-            keyColor = "\\u001b[38;2;0;255;175";
         }
         {
             type = "battery";
             key = "   Battery";
-            keyColor = "\\u001b[38;2;0;255;175";
             temp = true;
         }
         {
             type= "swap";
             key= "  󰓡 Swap";
-            keyColor= "\\u001b[38;2;0;255;175";
         }
         {
             type = "custom";
             format = "   PhysicalDisk";
-            outputColor= "\\u001b[38;2;0;255;175";
         }
         {
             type = "physicaldisk";
             key = "    󰋊 Drive";
-            keyColor= "\\u001b[38;2;0;255;175";
-            format= "[\\u001b[38;2;0;192;255m{3}\\u001b[0m|\\u001b[38;2;0;192;255m{4}\\u001b[0m]{2} [\\u001b[38;2;255;0;255m{7}\\u001b[0m|\\u001b[38;2;255;0;255m{8}\\u001b[0m] (\\u001b[38;2;0;255;0m{1}\\u001b[0m) - \\u001b[38;2;255;255;0m{5}\\u001b[0m";
             temp= true;
         }
         {
             type= "custom";
             format= "  󰪶 MountedFileSystems";
-            outputColor= "\\u001b[38;2;0;255;175";
         }
         {
             type= "disk";
             key= "    󱁼 FileSystem";
-            keyColor= "\\u001b[38;2;0;255;175";
         }
         {
             type = "bluetooth";
             key = "  󰂯 Bluetooth";
-            keyColor = "\\u001b[38;2;0;255;175";
         }
         {
             type= "monitor";
             key= "  󰹑 Monitor";
-            keyColor= "\\u001b[38;2;0;255;175";
         }
         {
             type= "brightness";
             key= "    󰃠 Brightness";
-            keyColor= "\\u001b[38;2;0;255;175";
         }
     	];
 		};
