a28a09f8f7f9012926141d73c89232cddc7bc048
c5514 <c5514@c5514.com>
2024-10-20 15:58:30 -0500

    Second attempt: ISO image

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -2,39 +2,20 @@
 {
 	imports = [
 		"${modulesPath}/installer/cd-dvd/installation-cd-graphical-gnome.nix"
-		"${modulesPath}/installer/cd-dvd/channel.nix"
 	];
 	nixpkgs = {
 		hostPlatform = lib.mkDefault "x86_64-linux";
 		config.allowUnfree = true;
 	};
-	nix = {
-		settings.experimental-features = ["nix-command" "flakes"];
-		extraOptions = "experimental-features = nix-command flakes";
-	};
-	boot = {
-		kernelPackages = pkgs.linuxPackages_latest;
-		supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs"];
-	};
+	nix.settings.experimental-features = ["nix-command" "flakes"];
 	networking = {
 		hostName = "iso";
 	};
-	systemd = {
-		services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
-		targets = {
-    		sleep.enable = false;
-    		suspend.enable = false;
-    		hibernate.enable = false;
-    		hybrid-sleep.enable = false;
-		};
-	};
-
 	environment.systemPackages = with pkgs; [
 		neovim
 		disko
 		git
 		parted
 	];
-	# home-manager.users.nixos = import ./home.nix;
 	users.extraUsers.root.password = "nixos";
 }
