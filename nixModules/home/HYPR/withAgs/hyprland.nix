47f904894f73846e3fe76451dd26cc6fde3a1997
c5514 <c5514@c5514.com>
2024-10-21 16:16:15 -0500

    Small mistake

--- nixModules/home/HYPR/withAgs/hyprland.nix
+++ nixModules/home/HYPR/withAgs/hyprland.nix
@@ -154,7 +154,7 @@
 				"$mainMod, M, exec, ags -t quicksettings"
 				#Workspaces overview
 				# "$mainMod, R, overview:toggle"
-				"$mainMod, R, ags -t overview"
+				"$mainMod, R, exec, ags -t overview"
 				#Screenshot
 				", PRINT, exec, grimblast copy screen"
 				"$mainMod, PRINT, exec, grimblast copy active"
