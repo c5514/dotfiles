7122dd76ba0e7dee3af7c75ae5e7687c4fb37725
c5514 <c5514@c5514.com>
2024-10-27 11:06:00 -0500

    Changes to install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ home-manager switch --flake .
 TEXTEXT_URL="https://github.com/textext/textext/releases/download/1.10.2/TexText-Linux-1.10.2.zip"
 DOWNLOAD_DIR="$HOME/Downloads"
 INSTALL_DIR="$HOME/.config/inkscape/extensions"
-ZIP_FILE="$DOWNLOAD_DIR/TexText-Linux-1.10.2.zip"
 
 # Create download and installation directories if they don't exist
 mkdir -p "$DOWNLOAD_DIR"
@@ -19,28 +18,27 @@ mkdir -p "$INSTALL_DIR"
 
 # Download the TexText ZIP file
 echo "Downloading TexText extension..."
-if curl -L -o "$ZIP_FILE" "$TEXTEXT_URL"; then
-    echo "Download completed."
-else
-    echo "Error downloading the file. Please check the URL."
-    exit 1
-fi
+curl -L -o "$DOWNLOAD_DIR/TexText-Linux-1.10.2.zip" "$TEXTEXT_URL"
+ZIP_FILE=$(find "$DOWNLOAD_DIR" -name "*.zip" -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f2-)
+echo "Download completed: $ZIP_FILE"
 
 # Unzip the downloaded file
 echo "Extracting TexText extension..."
-if unzip -o "$ZIP_FILE" -d "$DOWNLOAD_DIR"; then
-    echo "Extraction completed."
-else
-    echo "Error extracting the ZIP file."
-    exit 1
-fi
+unzip -o "$ZIP_FILE" -d "$DOWNLOAD_DIR"
+# Get the modification time of the ZIP file
+ZIP_MOD_TIME=$(stat -c %Y "$ZIP_FILE")
 
-# Move the extracted files to
+# Find the most recently extracted directory
+EXTRACTED_DIR=$(find "$DOWNLOAD_DIR" -maxdepth 1 -type d -newermt "@$ZIP_MOD_TIME" -printf "%p\n" | sort | tail -n 1)
+echo "Extraction completed."
+
+# Install the TexText extension
 echo "Installing TexText extension..."
-cd "$DOWNLOAD_DIR" || exit
+cd "$EXTRACTED_DIR" || exit
 python3 setup.py --skip-requirements-check
+
 # Clean up
 echo "Cleaning up..."
-rm -rf "$ZIP_FILE" "$DOWNLOAD_DIR/TexText-Linux-1.10.2"
+rm -rf "$ZIP_FILE" "$EXTRACTED_DIR"
 
 echo "TexText extension installed successfully!"
--- nixModules/home/theme.nix
+++ nixModules/home/theme.nix
@@ -1,13 +1,21 @@
 {pkgs, ...}:
 {
-	gtk.enable = true;
-	gtk.cursorTheme.package = pkgs.bibata-cursors;
-	gtk.cursorTheme.name = "Bibata-Modern-Ice";
-	gtk.cursorTheme.size = 24;
-	gtk.theme.package = pkgs.adw-gtk3;
-	gtk.theme.name = "adw-gtk3-dark";
-	gtk.iconTheme.package = pkgs.papirus-icon-theme;
-	gtk.iconTheme.name = "Papirus";
+	gtk = {
+		enable = true;
+		theme = {
+			name = "adw-gtk3-dark";
+			package = pkgs.adw-gtk3;
+		};
+		cursorTheme = {
+			package = pkgs.bibata-cursors;
+			name = "Bibata-Modern-Ice";
+			size = 24;
+		};
+		iconTheme = {
+			package = pkgs.papirus-icon-theme;
+			name = "Papirus";
+		};
+	};
 	qt = {
     	enable = true;
     	platformTheme.name = "gtk3";
