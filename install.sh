4725dfa56a5c0c6559507693ea72fe8980e99413
c5514 <cesaroctaviolevanoapari@gmail.com>
2024-12-30 08:43:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,69 @@
 #!/bin/bash
 echo "Installing packages"
 sudo apt update
-sudo apt install git gnome-themes-extra npm python3 gtk2-engines-murrine sassc zathura texlive-science texlive-latex-extra texlive-publishers texlive-fonts-extra texlive-bibtex-extra texlive-fonts-recommended make perl-tk kitty fish latexmk
+sudo apt install git npm python3 sassc fish escputil gh printer-driver-escpr
+echo "Installing LaTeX dependecies"
+sudo apt install texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-fonts-extra-links texlive-fonts-extra texlive-fonts-recommended texlive-formats-extra texlive-lang-english texlive-lang-spanish texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-lualatex texlive-pictures texlive-plain-generic texlive-publishers texlive-science texlive-xetex latexmk
+echo "Installing apps used with neovim"
+sudo apt install zathura zathura-pdf-poppler tmux 
+echo "To install inkscape answer the following question"
+while true; do
+    read -p "Are you in a Ubuntu based distro? (y/n): " answer
 
-echo "Installing Orchis theme"
-git clone http://github.com/vinceliuice/Orchis-theme.git
-cd "Orchis-theme" || exit
-./install.sh
+    case "$answer" in
+        [yY] | [yY][eE][sS])
+            echo "Adding latest version of inkscape to repository"
+            sudo add-apt-repository ppa:inkscape.dev/stable
+            sudo apt update
+            echo "Installing inkscape"
+            sudo apt install inkscape
+            TEXTEXT_URL="https://github.com/textext/textext/releases/download/1.10.2/TexText-Linux-1.10.2.zip"
+            DOWNLOAD_DIR="$HOME/Downloads"
+            INSTALL_DIR="$HOME/.config/inkscape/extensions"
+            ZIP_FILE="$DOWNLOAD_DIR/TexText-Linux-1.10.2.zip"
+            mkdir -p "$DOWNLOAD_DIR"
+            mkdir -p "$INSTALL_DIR"
 
-echo "Installing Tela circle icon theme"
-git clone http://github.com/vinceliuice/Tela-circle-icon-theme.git
-cd "Tela-circle-icon-theme" || exit
-./install.sh
-cd ..
-rm -rf "Tela-circle-icon-theme"
+            echo "Downloading TexText extension"
+            if curl -L -o "$ZIP_FILE" "$TEXTEXT_URL"; then
+                echo "Download completed."
+            else
+                echo "Error downloading the file. Please check the URL."
+                exit 1
+            fi
 
-#Installing neovim
-echo "Installing neovim"
+            echo "Extracting files"
+            if unzip -o "$ZIP_FILE" -d "$DOWNLOAD_DIR"; then
+                echo "Extraction completed."
+            else
+                echo "Error extracting the ZIP file."
+                exit 1
+            fi
+
+            echo "Installing TexText extension"
+            cd "$HOME/Downloads/textext-1.10.2/" || exit
+            python3 setup.py --skip-requirements-check
+            
+            echo "Cleaning up..."
+            rm -rf "$ZIP_FILE" "$DOWNLOAD_DIR/textext-1.10.2"
+
+            echo "TexText extension installed successfully!"
+            break
+            ;;
+        [nN] | [nN][oO])
+            echo "Installing inkscape and textext extension"
+            sudo apt install inkscape inkscape-textext
+            break
+            ;;
+        *)
+            echo "Invalid entry. Try again!"
+            ;;
+    esac
+done
+
+
+echo "Installing latest version of neovim"
 sudo apt install -y ninja-build gettext cmake unzip curl
-
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 wget "https://github.com/neovim/neovim/releases/download/$LATEST_RELEASE/nvim-linux64.tar.gz"
 tar xzf "nvim-linux64.tar.gz"
@@ -26,8 +71,7 @@ sudo mv nvim-linux64/bin/nvim /usr/local/bin/
 sudo mv nvim-linux64/share/nvim /usr/local/share/
 sudo mv nvim-linux64/man/man1/nvim.1 /usr/local/share/man/man1/
 rm -rf nvim-linux64*
-
-echo "Adding neovim configuration"
+echo "Downloading neovim configuration"
 git clone http://github.com/c5514/nvim_config.git
 cd "nvim_config" || exit
 mv nvim ~/.config
@@ -35,10 +79,10 @@ mv zathura ~/.config
 cd ..
 rm -rf "nvim_config"
 
-echo "Adding JetBrainsMono Nerd Font"
-bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)" <<EOF
-28
-EOF
+#echo "Adding JetBrainsMono Nerd Font"
+#bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)" <<EOF
+#28
+#EOF
 
 
 echo "Adding new repository for wezterm"
@@ -51,6 +95,25 @@ sudo apt install wezterm
 echo "Installing starship"
 curl -sS https://starship.rs/install.sh | sh
 echo "Adding starship configuration"
+
+echo "Installing zoxide"
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+echo "Installing yazi"
+
+echo "Installing lazygit"
+
+echo "Installing Orchis theme"
+git clone http://github.com/vinceliuice/Orchis-theme.git
+cd "Orchis-theme" || exit
+./install.sh
+
+echo "Installing Tela circle icon theme"
+git clone http://github.com/vinceliuice/Tela-circle-icon-theme.git
+cd "Tela-circle-icon-theme" || exit
+./install.sh
+cd ..
+rm -rf "Tela-circle-icon-theme"
 # TODO: Need to finish 
 
 # ALIAS_COMMAND="alias nv='nvim'"
@@ -68,53 +131,3 @@ echo "Adding starship configuration"
 # echo "Run 'source ~/.bashrc' to apply the change"
 # source ~/.bashrc
 
-echo "Installing Elegant-grub2-themes"
-git clone https://github.com/vinceliuice/Elegant-grub2-themes.git
-cd "Elegant-grub2-themes" || exit
-sudo ./install.sh -t mojave -s 1080p -l system
-
-
-
-echo "Updating to install inkscape"
-sudo add-apt-repository ppa:inkscape.dev/stable
-sudo apt update
-sudo apt install inkscape
-
-
-# Variables
-TEXTEXT_URL="https://github.com/textext/textext/releases/download/1.10.2/TexText-Linux-1.10.2.zip"
-DOWNLOAD_DIR="$HOME/Downloads"
-INSTALL_DIR="$HOME/.config/inkscape/extensions"
-ZIP_FILE="$DOWNLOAD_DIR/TexText-Linux-1.10.2.zip"
-
-# Create download and installation directories if they don't exist
-mkdir -p "$DOWNLOAD_DIR"
-mkdir -p "$INSTALL_DIR"
-
-# Download the TexText ZIP file
-echo "Downloading TexText extension..."
-if curl -L -o "$ZIP_FILE" "$TEXTEXT_URL"; then
-    echo "Download completed."
-else
-    echo "Error downloading the file. Please check the URL."
-    exit 1
-fi
-
-# Unzip the downloaded file
-echo "Extracting TexText extension..."
-if unzip -o "$ZIP_FILE" -d "$DOWNLOAD_DIR"; then
-    echo "Extraction completed."
-else
-    echo "Error extracting the ZIP file."
-    exit 1
-fi
-
-# Move the extracted files to
-echo "Installing TexText extension..."
-cd "$HOME/Downloads/textext-1.10.2/" || exit
-python3 setup.py --skip-requirements-check
-# Clean up
-echo "Cleaning up..."
-rm -rf "$ZIP_FILE" "$DOWNLOAD_DIR/textext-1.10.2"
-
-echo "TexText extension installed successfully!"
