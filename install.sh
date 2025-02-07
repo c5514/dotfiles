af1bb055c7f2d8e387b17c91ef62e07ba50ffd56
c5514 <cesaroctaviolevanoapari@gmail.com>
2025-02-06 21:24:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 echo "Installing packages..."
-sudo dnf install fish python3 pip
+sudo dnf install fish python3 pip gnome-tweaks
 
 echo "Installing Orchis theme"
 git clone http://github.com/vinceliuice/Orchis-theme.git
@@ -22,38 +22,24 @@ sudo dnf copr enable peterwu/rendezvous
 sudo dnf install bibata-cursor-themes
 
 echo "Configuring terminal"
-sudo dnf install eza
+sudo dnf install eza zoxide
 curl -sS https://starship.rs/install.sh | sh
-sudo dnf copr enable wezfurlong/wezterm-nightly
-sudo dnf install wezterm
-#
-# echo "Installing Hyprland and some dependencies..."
-# sudo dnf copr enable solopasha/hyprland
-# sudo dnf install hyprland cliphist swwww hypridle hyprlock
-
-# echo "Installing pywal and hyprshade..."
-# pip install --user pywal
-# sudo dnf install pipx
-# pipx ensurepath
-# sudo pipx ensurepath --global
-# pipx install hyprshade
-
-echo "Installing fonts..."
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CaskaydiaMono.tar.xz
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Noto.tar.xz
+sudo dnf copr enable pgdev/ghostty
+sudo dnf install ghostty
 
 echo "Installing neovim and LaTex related dependencies..."
 sudo dnf install nvim zathura zathura-poppler-pdf texlive-scheme-full inkscape
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install lazygit
 git clone https://github.com/c5514/nvim_config.git
 mv nvim ~/.config
 mv zathura ~/.config
 
 # Variables
-TEXTEXT_URL="https://github.com/textext/textext/releases/download/1.10.2/TexText-Linux-1.10.2.zip"
+TEXTEXT_URL="https://github.com/textext/textext/releases/download/1.11.0/TexText-Linux-1.11.0.zip"
 DOWNLOAD_DIR="$HOME/Downloads"
 INSTALL_DIR="$HOME/.config/inkscape/extensions"
-ZIP_FILE="$DOWNLOAD_DIR/TexText-Linux-1.10.2.zip"
+ZIP_FILE="$DOWNLOAD_DIR/TexText-Linux-1.11.0.zip"
 
 # Create download and installation directories if they don't exist
 mkdir -p "$DOWNLOAD_DIR"
@@ -79,7 +65,7 @@ fi
 
 # Move the extracted files to
 echo "Installing TexText extension..."
-cd "$HOME/Downloads/textext-1.10.2/" || exit
+cd "$HOME/Downloads/textext-1.11.0/" || exit
 python3 setup.py --skip-requirements-check
 # Clean up
 echo "Cleaning up..."
@@ -89,6 +75,3 @@ echo "TexText extension installed successfully!"
 # TODO: Need to add spotify installation via flatpak and spicetify
 # Need to add vesktop
 # And extensions and gnome-tweaks installation. I am not sure how to configure gnome via dconf
-
-echo "Changing shell to fish"
-chsh -s /usr/bin/fish
