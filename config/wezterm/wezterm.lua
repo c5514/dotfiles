25cacbcfc2fcdd397b86b120f4bf2a51ecc0bf58
c5514 <cesaroctaviolevanoapari@gmail.com>
2025-05-18 14:12:01 -0500

    Update wezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
-config.window_background_opacity = 0.95
+-- config.window_background_opacity = 0.95
 config.hide_tab_bar_if_only_one_tab = true
--- config.window_decorations = "NONE"
+config.window_decorations = "NONE"
 config.cell_width = 0.90
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
@@ -15,23 +16,21 @@ config.inactive_pane_hsb = {
   saturation = 0.9,
   brightness = 0.8,
 }
+config.color_scheme = 'Dark+'
 config.colors = {
-  background = "#000000",
+  background = "#0c090a",
   foreground = "#b2b5b3",
   cursor_bg = "#b2b5b3",
   cursor_fg = "#000000",
   cursor_border = "#52ad70",
   selection_fg = "black",
 }
+config.initial_rows = 50
+config.initial_cols = 100
 config.default_cursor_style = "BlinkingBar"
-config.font_size = 15
+config.font_size = 14
 config.font = wezterm.font_with_fallback({
-  -- {
-  -- 	family = "Noto Sans Mono",
-  -- 	weight = "Medium",
-  -- 	italic = false,
-  -- },
-  "CaskaydiaMono NF",
+  "JetBrainsMono NF",
 })
 config.font_rules = {
   {
@@ -62,5 +61,71 @@ config.font_rules = {
     }),
   },
 }
-config.default_prog = { "tmux" }
+config.keys = {
+  {
+    key = 'l',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Left',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'h',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'j',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'k',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Up',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'h', mods = 'CTRL|ALT', action = act.AdjustPaneSize { 'Left', 5 },
+  },
+  {
+    key = 'j', mods = 'CTRL|ALT', action = act.AdjustPaneSize { 'Down', 5 },
+  },
+  { key = 'k', mods = 'CTRL|ALT', action = act.AdjustPaneSize { 'Up', 5 } },
+  {
+    key = 'l', mods = 'CTRL|ALT', action = act.AdjustPaneSize { 'Right', 5 },
+  },
+  {
+    key = 'd',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'd',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+}
+config.default_prog = { "/usr/bin/fish", '-l' }
 return config
