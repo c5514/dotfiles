5c522f9d201476c2f4fef01bac8a8f96112a90c5
c5514 <c5514@c5514.com>
2025-08-06 10:52:23 -0500

    Rofi applets and tmux config

---
+++ config/rofi/clipboard/cliphist.sh
@@ -0,0 +1,1 @@
+rofi -theme ~/.config/rofi/clipboard/clipboard.rasi -modi clipboard:~/.config/rofi/clipboard/clipboard.sh -show clipboard
---
+++ config/rofi/launcher/launcher.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+dir="$HOME/.config/rofi/launcher/launcher"
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}.rasi
